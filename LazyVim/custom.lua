1328cf421778051cb2ff4f1e6ff4abdc086d9365
Dima Chumak <chumakd@gmail.com>
2024-04-29 11:59:33 +0200

    Add custom configs for LazyVim

---
+++ LazyVim/custom.lua
@@ -0,0 +1,56 @@
+-- In your plugin files, you can:
+-- * add extra plugins
+-- * disable/enabled LazyVim plugins
+-- * override the configuration of LazyVim plugins
+return {
+  -- use lates version
+  { "folke/lazy.nvim", version = false },
+  { "LazyVim/LazyVim", version = false },
+
+  -- disable default plugins
+  { "akinsho/bufferline.nvim", enabled = false },
+  { "folke/noice.nvim", enabled = false },
+  { "rcarriga/nvim-notify", enabled = false },
+
+  -- extra plugins
+  { import = "lazyvim.plugins.extras.editor.aerial" },
+  { import = "lazyvim.plugins.extras.editor.harpoon2" },
+  { import = "lazyvim.plugins.extras.editor.outline" },
+
+  -- lang support
+  { import = "lazyvim.plugins.extras.lang.ansible" },
+  --{ import = "lazyvim.plugins.extras.lang.clangd" },
+  --{ import = "lazyvim.plugins.extras.lang.cmake" },
+  { import = "lazyvim.plugins.extras.lang.docker" },
+  { import = "lazyvim.plugins.extras.lang.elixir" },
+  { import = "lazyvim.plugins.extras.lang.go" },
+  { import = "lazyvim.plugins.extras.lang.json" },
+  { import = "lazyvim.plugins.extras.lang.markdown" },
+  { import = "lazyvim.plugins.extras.lang.python" },
+  { import = "lazyvim.plugins.extras.lang.ruby" },
+  { import = "lazyvim.plugins.extras.lang.rust" },
+  { import = "lazyvim.plugins.extras.lang.tailwind" },
+  { import = "lazyvim.plugins.extras.lang.yaml" },
+
+  -- add more treesitter parsers (extending default config)
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      -- add tsx and treesitter
+      vim.list_extend(opts.ensure_installed, {
+        "tsx",
+        "typescript",
+      })
+    end,
+  },
+
+  {
+    "nvim-lualine/lualine.nvim",
+    event = "VeryLazy",
+    opts = function()
+      return {
+        --[[add your custom lualine config here]]
+      }
+    end,
+  },
+}
